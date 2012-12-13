265aaaaf186d4abfaf086dd5e81b94ed30f466c5
Dominik Bonsch <dominik.bonsch@webfrap.net>
2012-12-13 12:12:53 +0100

    extended deployment script

--- bash/deployment/db/db_sync_database.sh
+++ bash/deployment/db/db_sync_database.sh
@@ -4,9 +4,21 @@
 # jump in the gateway
 cd "${deplPath}/${gatewayName}"
 
+if [ "force" == "$syncType" ]; then
+  
+  /usr/bin/php ./cli.php Daidalos.deploy.syncDatabase sync_col=true delete_col=true sync_table=true
+  
+elif [ "auto" == "$syncType" ]; then
+
+  /usr/bin/php ./cli.php Daidalos.deploy.syncDatabase sync_col=true 
+
+else
+  
+  /usr/bin/php ./cli.php Daidalos.deploy.syncDatabase
+  
+fi
 
 
-/usr/bin/php ./cli.php Daidalos.deploy.syncDatabase sync_col=true delete_col=true sync_table=true
 /usr/bin/php ./cli.php Daidalos.deploy.syncData
 /usr/bin/php ./cli.php Daidalos.deploy.syncMetaData
 
