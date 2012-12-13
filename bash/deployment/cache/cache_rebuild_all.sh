5488d7a0845235b9788109b6a89f3fd18236d54f
Dominik Bonsch <dominik.bonsch@webfrap.net>
2012-12-13 16:19:33 +0100

    code cosmetics

--- bash/deployment/cache/cache_rebuild_all.sh
+++ bash/deployment/cache/cache_rebuild_all.sh
@@ -6,8 +6,6 @@ writeLn "Rebuild the CSS / JS and Theme cache"
 # jump in the gateway
 cd "${deplPath}/${gatewayName}"
 
-
-
 /usr/bin/php ./cli.php Webfrap.cache.rebuildAllCss
 /usr/bin/php ./cli.php Webfrap.cache.rebuildAllJs
 /usr/bin/php ./cli.php Webfrap.cache.rebuildAllTheme
