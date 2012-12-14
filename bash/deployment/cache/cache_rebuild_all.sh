98ea695a8232e484f1188c3d6009e9e2fcc6a091
Dominik Bonsch <dominik.bonsch@webfrap.net>
2012-12-14 13:14:18 +0100

    added fix for the permissions

--- bash/deployment/cache/cache_rebuild_all.sh
+++ bash/deployment/cache/cache_rebuild_all.sh
@@ -7,9 +7,16 @@ writeLn "Rebuild the CSS / JS and Theme cache"
 cd "${deplPath}/${gatewayName}"
 
 /usr/bin/php ./cli.php Webfrap.cache.rebuildAllCss
+chown -R "$codeOwner" "${deplPath}/${gatewayName}/cache/css"
+
 /usr/bin/php ./cli.php Webfrap.cache.rebuildAllJs
+chown -R "$codeOwner" "${deplPath}/${gatewayName}/cache/javascript"
+
 /usr/bin/php ./cli.php Webfrap.cache.rebuildAllAppTheme
+chown -R "$codeOwner" "${deplPath}/${gatewayName}/cache/app_theme"
+
 /usr/bin/php ./cli.php Webfrap.cache.rebuildAllWebTheme
+chown -R "$codeOwner" "${deplPath}/${gatewayName}/cache/web_theme"
 
 # jump back to the package path
 cd $packagePath
